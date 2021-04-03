bac118c01d3a1d697e325b3b3a848bf64705db91
naeth <81841076+NotNaeth@users.noreply.github.com>
2021-04-02 23:09:59 -0400

    Updated.

---
+++ .github/workflows/READ.lua
@@ -0,0 +1,16 @@
+-- **DISCLAIMER** THIS IS FOR EDUCATIONAL PURPOSES ONLY!
+
+-- The new, easier way of exploiting. Now with Anima. 
+-- Anima is an "Online Exploit Hub" that eases the process of learning how to code and how to exploit.
+
+-- Please note, this process WILL use your IP. This gives you a better experience!
+-- *breath in* *breath out*
+
+-- Made by naeth#0001!
+
+-- Do NOT leak this, as it is a paid use. If leaked, legal action will be necessary.
+
+
+-- Thanks so much for buying Anima! Have fun scripting!
+
+print('autoexecute ERD made by naeth#0001')
